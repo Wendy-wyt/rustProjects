248aee98509dd220516bbc8c18d9e1c695a7e28a
Wendy-wyt <wuyuting0101@gmail.com>
2023-10-17 00:32:41 -0400

    allow users to give input through system input

--- Compression/src/main.rs
+++ Compression/src/main.rs
@@ -3,33 +3,37 @@ extern crate flate2;
 
 use flate2::write::GzEncoder;
 use flate2::Compression;
-use std::env::args;
 use std::fs::File;
 use std::io::copy;
 use std::io::BufReader;
+use std::io::stdin;
 use std::time::Instant; // used to record the time taken to compress/decompress file
 
 fn main(){
-    // check if the program has received enough arguments
-    if args().len() !=3 {
-        eprintln!("Usage: 'source' 'target'");
-        return;
-    }
     // parse arguments to input path and output path
-    let mut input=BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    let output=File::create(args().nth(2).unwrap()).unwrap();
+    println!("Please enter the input file: ");
+    let mut input_path=String::new();
+    stdin().read_line(&mut input_path).unwrap();
+
+    let mut input_file=BufReader::new(File::open(input_path).unwrap());
+
+    println!("Please enter the output file: ");
+    let mut output_path=String::new();
+    stdin().read_line(&mut output_path).unwrap();
+
+    let output_file=File::create(output_path).unwrap();
     // create a new encoder
-    let mut encoder = GzEncoder::new(output, Compression::default());
+    let mut encoder = GzEncoder::new(&output_file, Compression::default());
     // timing
     let start=Instant::now();
     // pass input file to encoder
-    copy(&mut input, &mut encoder).unwrap();
+    copy(&mut input_file, &mut encoder).unwrap();
     // formatted print
     println!(
         "Source len: {:?}",
-        input.get_ref().metadata().unwrap().len()
+        input_file.get_ref().metadata().unwrap().len()
     );
-    println!("Target len: {:?}", output.metadata().unwrap().len());
+    println!("Target len: {:?}", (&output_file).metadata().unwrap().len());
     // print the time elapsed
     println!("Elapsed: {:?}", start.elapsed());
 
